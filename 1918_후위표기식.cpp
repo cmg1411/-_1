9b7474e21b9a57944018c8aa29a4880ad980780d
mingoer <cmg14111@gmail.com>
2020-10-17 22:41:53 +0900

    days4

--- 1918_후위표기식.cpp
+++ 1918_후위표기식.cpp
@@ -1,28 +1,44 @@
 #include<iostream>
 #include<stack>
-#include<vector>
 #include<string>
-#include<algorithm>
 using namespace std;
 
-int main(){
+int cmdpre(char cmd){
+    if(cmd=='+'||cmd=='-') return 1;
+    else if(cmd=='*'||cmd=='/') return 2;
+    else return 0;
+}
 
-    stack<char> alp, cmd;
-    string str;
+stack<char> s;
+
+int main (){
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
 
     char c;
-    while(cin>>c){
-        if(c=='(') continue;
-        if(c==')'){
-
-        }
-
-        if(c>='A'&&c<='Z'){
-            alp.push(c);
+    while(cin >> c){
+        if(c>='A'&&c<='Z') cout << c;
+        else if(c=='(') s.push(c);
+        else if(c==')'){
+            while(s.top()!='('){
+                cout << s.top();
+                s.pop();
+            }
+            s.pop();
         }else{
-            cmd.push(c);
+            while(!s.empty() && (cmdpre(c)<=cmdpre(s.top()))){ // ( 를 우선순위 1로 지정했기 때문에 s.top()에서 ( 를 만나면 무조건 그만한다.
+                cout << s.top();
+                s.pop();
+            }
+            s.push(c);
+            
         }
     }
-    a
+
+    while(!s.empty()){
+        cout << s.top();
+        s.pop();
+    }
     return 0;
 }
